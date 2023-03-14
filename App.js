d615aa4e7be7d7d53ae0ebb4c215474123c3f183
Carlo Conte <carlo.conte@gmail.com>
2023-03-14 08:23:01 -0300

    Finalizando o projeto 1

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   Montserrat_400Regular, 
   Montserrat_700Bold 
 } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 
 import Cesta from './src/telas/Cesta';
 import mock from './src/mocks/cesta';
@@ -16,11 +17,11 @@ export default function App() {
   });
 
   if (!fonteCarregada) {
-    return <View />
+    return <AppLoading />
   }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={ { flex: 1 } }>
       <StatusBar />
       <Cesta {...mock} />
     </SafeAreaView>
